b59e7d7cf67ae72f1c6e21ba8b7d125097e9fc29
dankeone <sg10110000@gmail.com>
2020-03-22 13:54:33 +0300

    fix: add new dream-team.js

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,9 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let res="";
+  if (!Array.isArray(members)) return(false)	; else{
+ 	for(let i = 0; i < members.length; i++) 
+ 		if (typeof(members[i]) == "string")
+ 		res += members[i][members[i].search(/[a-z]/i)];
+  }
+ return(res.toUpperCase().split("").sort().join(""));
 };
