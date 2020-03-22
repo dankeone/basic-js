73e4a709fd98c523616e447b31d6e6a2e59b82a0
dankeone <sg10110000@gmail.com>
2020-03-22 22:03:50 +0300

    fix: add new what-season.js

--- src/what-season.js
+++ src/what-season.js
@@ -6,10 +6,10 @@ module.exports = function getSeason(date) {
 	catch(err) {
 		throw Error;
 	}
-	const m = date.getMonth();
-	if (m >= 2 && m <= 4) return ('spring'); else
-	if (m >= 5 && m <= 7) return ('summer'); else
-	if (m >= 8 && m <= 10) return ('fall'); else
-	if (m >= 11 || m <= 1) return ('winter');  
+	let s = date.getMonth();
+	if (s >= 2 && s <= 4) return ('spring'); else
+	if (s >= 5 && s <= 7) return ('summer'); else
+	if (s >= 8 && s <= 10) return ('fall'); else
+	if (s >= 11 || s <= 1) return ('winter');  
   // remove line with error and write your code here
 };
