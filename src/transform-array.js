8f402ff39cbdf51a1db12f794c22b81ae24673e6
dankeone <sg10110000@gmail.com>
2020-03-22 23:11:08 +0300

    fix: add new transform-array.js

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,19 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
+module.exports = function transform(arr) {
+    if (!Array.isArray(arr)) throw new Error()
+    let res = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] == "--discard-next") {
+            i++;
+        } else if (arr[i] == "--discard-prev") {
+            res.pop();
+        } else if (arr[i] == "--double-next" && i + 1 != arr.length) {
+            res.push(arr[i + 1]);
+        } else if (arr[i] == "--double-prev" && i - 1 > 0) {
+            res.push(arr[i - 1]);
+        } else if (arr[i] != "--discard-prev" && arr[i] != "--double-prev" && arr[i] != "--double-next" && arr[i] != "--discard-next") {
+            res.push(arr[i]);
+        }
+    }
+    return(res);
     // remove line with error and write your code here
 };
